ba18c6e044d43e5e38c7896d02a7cb5e76c5640b
Jackie Merritt <remmerritt@gmail.com>
2025-06-24 17:10:24 -0400

    Assignment complete

---
+++ code.py
@@ -0,0 +1,15 @@
+from adafruit_circuitplayground import cp
+import time
+BLACK = (0, 0, 0)
+while True:
+    for num in range(0, 10):
+        cp.pixels[num] = (0, 100, 100) #
+        time.sleep(.2)
+        cp.pixels[num] = BLACK
+        time.sleep(.2)
+
+    for num in range(9, -1, -1):
+        cp.pixels[num] = (150, 0, 150) #
+        time.sleep(.2)
+        cp.pixels[num] = BLACK
+        time.sleep(.2)
